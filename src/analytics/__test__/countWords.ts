234bbbe025a82ecc84515c3745419a83497cb70e
Guillem Blasco <gowet92@gmail.com>
2024-07-13 15:18:45 +0200

    Test function countWords

---
+++ src/analytics/__test__/countWords.ts
@@ -0,0 +1,40 @@
+import { countWords } from "../../analytics/index.js";
+
+const test1 = (): void => {
+  console.log("countWords function case 0");
+
+  const word = "";
+  const wordsCount = countWords(word);
+  const expectedReturnWord = 0;
+
+  if (wordsCount === expectedReturnWord) {
+    console.log("Test Passed");
+
+    return;
+  } else {
+    console.log("Test Failed");
+
+    return;
+  }
+};
+
+const test2 = (): void => {
+  console.log("countWords function case 3");
+
+  const words = "hola como estas";
+  const wordsCount = countWords(words);
+  const expecedWord = 3;
+
+  if (wordsCount === expecedWord) {
+    console.log("Test Passed");
+
+    return;
+  } else {
+    console.log("Test Failed");
+
+    return;
+  }
+};
+
+test1();
+test2();
