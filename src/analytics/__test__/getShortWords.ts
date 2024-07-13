0903b039a5e9053b6760707dfc07019fd22786ee
Guillem Blasco <gowet92@gmail.com>
2024-07-13 15:37:26 +0200

    Test function getShortWords

---
+++ src/analytics/__test__/getShortWords.ts
@@ -0,0 +1,40 @@
+import { getShortWords } from "../../analytics/index.js";
+
+const test1 = (): void => {
+  console.log("should return 0 when function recive empty string ");
+
+  const shortWord = "";
+  const shortWordsCount = getShortWords(shortWord);
+  const expectedShortWord = 0;
+
+  if (shortWordsCount === expectedShortWord) {
+    console.log("Test Passed");
+
+    return;
+  } else {
+    console.log("Test Failed");
+
+    return;
+  }
+};
+
+const test2 = (): void => {
+  console.log("should be return number of shorts words");
+
+  const shortWord = "hola como estas giliiii";
+  const shortWordsCount = getShortWords(shortWord);
+  const expectedShortWord = 2;
+
+  if (shortWordsCount === expectedShortWord) {
+    console.log("Test Passed");
+
+    return;
+  } else {
+    console.log("Test Failed");
+
+    return;
+  }
+};
+
+test1();
+test2();
