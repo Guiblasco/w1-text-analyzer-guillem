b0890f455a106825bda29734f5a3aa940d9249d6
Guillem Blasco <gowet92@gmail.com>
2024-07-13 15:22:47 +0200

    Test function countCharacters

---
+++ src/analytics/__test__/countCharacters.ts
@@ -0,0 +1,39 @@
+import { countCharacters } from "../../analytics/index.js";
+
+const test1 = (): void => {
+  console.log("countCharacters Function test 1");
+
+  const wordsCount = "";
+  const charactersCount = countCharacters(wordsCount);
+  const expectedResult = 0;
+
+  if (charactersCount === expectedResult) {
+    console.log("Test Passed");
+
+    return;
+  } else {
+    console.log("Test Failed");
+
+    return;
+  }
+};
+
+const test2 = (): void => {
+  console.log("countCharacters Function test 3");
+
+  const wordsCount = "hola como estas";
+  const charactersCount = countCharacters(wordsCount);
+  const expectedResult = 15;
+
+  if (charactersCount === expectedResult) {
+    console.log("Test Passed");
+
+    return;
+  } else {
+    console.log("Test Failed");
+
+    return;
+  }
+};
+test1();
+test2();
