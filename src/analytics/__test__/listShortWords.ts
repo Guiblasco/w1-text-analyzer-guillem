f50167a338cd6a7d88c86463ea164011050ce3bc
Guillem Blasco <gowet92@gmail.com>
2024-07-13 15:41:52 +0200

    Test function listShortWords

---
+++ src/analytics/__test__/listShortWords.ts
@@ -0,0 +1,39 @@
+import { listShortWords } from "../../analytics/index.js";
+
+const test1 = (): void => {
+  console.log("\nshould returns 0 words if fuction empty string is given");
+
+  const phrase = "";
+  const listWords = listShortWords(phrase);
+  const expectedResult = 0;
+
+  if (listWords.length === expectedResult) {
+    console.log("Test Passes");
+
+    return;
+  } else {
+    console.log("Test Fails");
+
+    return;
+  }
+};
+
+const test2 = (): void => {
+  console.log("\nShould return a words list separated with commas");
+
+  const phrase = "Hola que tal estas ornitorrinco";
+  const listWords = listShortWords(phrase);
+  const expectedResult = "Hola,que,tal";
+
+  if (listWords === expectedResult) {
+    console.log("Test Passes");
+
+    return;
+  } else {
+    console.log("Test Fails");
+
+    return;
+  }
+};
+test1();
+test2();
